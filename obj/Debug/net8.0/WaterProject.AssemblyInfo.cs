69413b45731286ee5b7aa851c88059e342fc6ca0
sethbr11 <sethabrock@gmail.com>
2024-04-02 14:58:34 -0600

    Some changes

--- obj/Debug/net8.0/WaterProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/WaterProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21212d821306214a2cdbc72a855bafe87b050156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a100c9ad4cf01ea755467cffaa3979a0e0c9128")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
