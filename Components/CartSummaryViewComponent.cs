dc9e0029658700a7ed5232117862318f2cbed1b3
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-02 14:59:39 -0600

    Some changes

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WaterProject.Models;
+
+namespace WaterProject.Components {
+    public class CartSummaryViewComponent : ViewComponent {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService) {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke() { 
+            return View(cart); 
+        }
+    }
+}
